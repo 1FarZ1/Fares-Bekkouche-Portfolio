a50ade6e28ba091435c83ac7c735231c43c130bb
1FarZ1 <fbekkouche149@gmail.com>
2024-06-07 19:08:18 +0100

    changing images

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -18,3 +18,17 @@ function getThemeOnLoad() {
 }
 
 getThemeOnLoad();
+
+
+new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
